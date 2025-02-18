e4a7f45fb06c1b0af11428dfb63774af921318e2
Aris Jirat <arisjiratkurniawan@gmail.com>
2025-02-18 13:35:29 +0700

    nvim and zsh

--- nvim/lua/arisjirat/plugins/flutter.lua
+++ nvim/lua/arisjirat/plugins/flutter.lua
@@ -8,7 +8,22 @@ return {
 	},
 	-- config = true,
 	config = function()
-		require("flutter-tools").setup({
+		local ft = require("flutter-tools").setup({
+			decorations = {
+				statusline = {
+					-- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+					-- this will show the current version of the flutter app from the pubspec.yaml file
+					app_version = true,
+					-- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+					-- this will show the currently running device if an application was started with a specific
+					-- device
+					device = true,
+					-- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
+					-- this will show the currently selected project configuration
+					project_config = true,
+				},
+			},
+
 			-- flutter_path = "~/fvm/default/bin/dart",
 			-- lsp = {
 			--   cmd = { "~/fvm/default/bin/dart", "language-server", "--protocol=lsp" },
@@ -25,8 +40,36 @@ return {
 					require("dap.ext.vscode").load_launchjs()
 				end,
 			},
+			dev_log = {
+				notify_errors = true,
+			},
+			outline = {
+				auto_open = true,
+			},
+
+			dev_tools = {
+				autostart = true,
+				auto_open_browser = true,
+			},
 		})
+
+		local map = vim.keymap.set
+
+		map("n", "<leader>rrd", "<cmd>FlutterDevices<CR>", { desc = "Run on device" })
+		map("n", "<leader>rrh", "<cmd>FlutterHotReload<CR>", { desc = "Hot reload" })
+		map("n", "<leader>rrs", "<cmd>FlutterHotRestart<CR>", { desc = "Hot restart" })
+		map("n", "<leader>rrQ", "<cmd>FlutterQuit<CR>", { desc = "Quit flutter app" })
+		map("n", "<leader>rrt", "<cmd>FlutterTest<CR>", { desc = "Run tests" })
+		-- log clear
+		map("n", "<leader>rrc", "<cmd>FlutterLogClear<CR>", { desc = "Clear log" })
+		-- toggle log
+		map("n", "<leader>rrg", "<cmd>FlutterLogToggle<CR>", { desc = "Toggle log" })
+		-- open dev tools
+		map("n", "<leader>rrb", "<cmd>FlutterDevTools<CR>", { desc = "Open dev tools" })
+		-- toggle outline
+		map("n", "<leader>rro", "<cmd>FlutterOutlineToggle<CR>", { desc = "Toggle outline" })
 	end,
+
 	-- config = function ()
 	--   require("flutter-tools").setup({
 	--     -- flutter_path = "~/fvm/default/bin/dart",
